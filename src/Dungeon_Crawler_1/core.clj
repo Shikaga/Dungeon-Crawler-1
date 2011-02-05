7c8a5d591e66bcc78e3e03a9418ec2565bc50a55
Jon <shikaga@shikaga-laptop.(none)>
2011-02-05 18:32:17 +0000

    Added leiningen files

---
+++ src/Dungeon_Crawler_1/core.clj
@@ -0,0 +1,1 @@
+(ns Dungeon-Crawler-1.core)
---
+++ test/Dungeon_Crawler_1/test/core.clj
@@ -0,0 +1,6 @@
+(ns Dungeon-Crawler-1.test.core
+  (:use [Dungeon-Crawler-1.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
