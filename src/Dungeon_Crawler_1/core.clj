e14fbf3429fdc9b0a6344bd10b46bc8c5689a5cc
Jon <shikaga@shikaga-laptop.(none)>
2011-02-07 21:53:40 +0000

    State added though barely functional

--- src/Dungeon_Crawler_1/core.clj
+++ src/Dungeon_Crawler_1/core.clj
@@ -1,11 +1,39 @@
 (ns Dungeon-Crawler-1.core)
-(defstruct player :name :location)
-(defstruct location :name :description)
+
+(defstruct player :name :location :inventory)
+(defstruct location :name :description :items)
 (defstruct item :name :description)
 (defstruct inventory :name :items)
-
-(defn incr [x] (+ x 1))
+(defstruct world :players :locations)
 
 (defn listItems [inventory] (print (inventory :items)))
+(defn addItem [inventory item] (dosync (ref-set inventory (update-in (deref inventory) [:items] #(conj % item)))))
+(defn pickupItem [player item] (update-in player [:inventory] #(addItem % item)))
+(defn getItems [room] (map :name (room :items))) ;Cool example
+(defn itemInLocation? [location item] (contains? (location :items) item))
+(defn removeItemFromLocation "returns removed item if present else empty list" [location item]
+  (if (itemInLocation? location item) (do (dosync (ref-set location (update-in (deref location) [:items] #(disj % item)))) true) false))
+(defn moveItemFromLocationToInventory [location inventory item] (if (removeItemFromLocation location item) (addItem inventory item) nil))
+(defn getItemFromName [items name] (first (filter #(not= (% :name) name) items)))
+(def goldkey (struct item "key" "opens a lock somewhere"))
+(def rope (struct item "rope" "just enough to hang yourself"))
 
-(defn addItems [inventory item] (update-in inventory [:items] #(conj % item)))
+(def player1Inventory (ref (struct inventory "You Inventory" '(goldkey))))
+(def myInventory2 (struct inventory "yi" '(goldkey)))
+(def outside (struct location "outside" "You are outside!" #{'rope}))
+(def manor (struct location "Manor" "You stand in a manor!" #{}))
+(def player1 (struct player "Jon" outside player1Inventory))
+(def initWorld (ref (struct world {:player1 player1} {:outside location})))
+(def currentRoom (ref outside))
+
+(defn goNorth [] (dosync (ref-set currentRoom manor)))
+
+(def flag true)
+(println "> ")
+(while flag
+  (def line (read-line))
+  (if (= line "lol") (println "no you!") ())
+  (if (= line "help") (println "RTFM noob!") ())
+  (if (= line "look") (println (currentRoom :description)))
+  (if (= line "north") (goNorth))
+)
--- test/Dungeon_Crawler_1/test/core.clj
+++ test/Dungeon_Crawler_1/test/core.clj
@@ -1,16 +1,17 @@
 (ns Dungeon-Crawler-1.test.core
   (:use [Dungeon-Crawler-1.core] :reload)
+  (:use [Dungeon-Crawler-1.world] :reload)
   (:use [clojure.test]))
 
 (def testLocation (struct location "testName" "testDescription"))
-
-(def testPlayer (struct player "Jon" testLocation))
 
 (def testItem1 (struct item "item1" "item of power!"))
 (def testItem2 (struct item "item2" "item of current!"))
 (def testItem3 (struct item "item3" "item of resistance!"))
 
 (def testInventory (struct inventory "testInventory" '(testItem1 testItem2)))
+
+(def testPlayer (struct player "Jon" testLocation testInventory))
 
 (deftest incr-works
   (is (= 2 (incr 1))))
@@ -26,3 +27,6 @@
 
 (deftest addItemsTest
   (is (= '(testItem3 testItem1 testItem2) ((addItems testInventory 'testItem3) :items))))
+
+(deftest pickupItemTest
+  (is (= '(testItem3 testItem1 testItem2) (((pickupItem testPlayer 'testItem3) :inventory) :items))))
